2f3d10e930fe5b6d3ef696a02c88a79324fd4d0e
root <root@DESKTOP-319LLRG.localdomain>
2024-01-31 10:33:19 +0700

    update kost

--- app/Http/Controllers/Api/KostController.php
+++ app/Http/Controllers/Api/KostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Helpers\ApiResponse;
 use App\Models\Kost;
 use App\Http\Requests\Kost\AddKostRequest;
+use App\Http\Requests\Kost\UpdateKostRequest;
 use App\Http\Resources\KostResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -344,6 +345,171 @@ class KostController extends Controller
         } catch (\Exception $e) {
             return ApiResponse::error(
                 message: "Failed to create kost: {$e->getMessage()}",
+                statusCode: Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    /**
+     * @OA\Put(
+     *     path="/api/kost/{kost}",
+     *     summary="Update a kost by ID",
+     *     description="Updates a kost by its ID",
+     *     tags={"Kost"},
+     *     @OA\Parameter(
+     *         name="kost",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the kost to update",
+     *         @OA\Schema(
+     *             type="integer"
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(ref="#/components/schemas/UpdateKostRequest")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Kost updated successfully",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Kost updated successfully"
+     *             ),
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="post",
+     *                     ref="#/components/schemas/KostResource"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Unauthorized"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Forbidden"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation error",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Validation error"
+     *             ),
+     *             @OA\Property(
+     *                 property="errors",
+     *                 type="object",
+     *                 description="Validation error details",
+     *                 example={
+     *                     "user_id": {
+     *                         "The user id field is required."
+     *                     },
+     *                     "title": {
+     *                         "The title field is required."
+     *                     },
+     *                     "description": {
+     *                         "The description field is required."
+     *                     },
+     *                     "tipe_sewa": {
+     *                         "The tipe sewa field is required."
+     *                     },
+     *                     "tahun_dibangun": {
+     *                         "The tahun dibangun field is required."
+     *                     },
+     *                     "alamat": {
+     *                         "The alamat field is required."
+     *                     },
+     *                     "provinsi": {
+     *                         "The provinsi field is required."
+     *                     },
+     *                     "kota": {
+     *                         "The kota field is required."
+     *                     },
+     *                     "kecamatan": {
+     *                         "The kecamatan field is required."
+     *                     },
+     *                     "panjang_kamar": {
+     *                         "The panjang kamar field is required."
+     *                     },
+     *                     "lebar_kamar": {
+     *                         "The lebar kamar field is required."
+     *                     },
+     *                     "total_kamar": {
+     *                         "The total kamar field is required."
+     *                     },
+     *                     "kamar_tersedia": {
+     *                         "The kamar tersedia field is required."
+     *                     },
+     *                     "harga": {
+     *                         "The harga field is required."
+     *                     },
+     *                     "status_biaya_tambahan": {
+     *                         "The status biaya tambahan field is required."
+     *                     },
+     *                     "created_at": {
+     *                         "The created at field is required."
+     *                     },
+     *                     "created_at": {
+     *                         "The created at field is required."
+     *                     },
+     *                     "created_by": {
+     *                         "The created by field is required."
+     *                     }
+     *                 }
+     *             )
+     *         )
+     *     ),
+     *     security={
+     *         {"bearerAuth": {}}
+     *     }
+     * )
+     */
+    public function update(UpdateKostRequest $request, Kost $kost)
+    {
+        try {
+            $validatedData = $request->validated();
+            $kost->update($validatedData);
+
+            return ApiResponse::success(
+                message: 'Kost updated successfully',
+                data: [
+                    "kost" => KostResource::make($kost->load('user'))
+                ],
+                statusCode: Response::HTTP_OK
+            );
+        } catch (\Exception $e) {
+            return ApiResponse::error(
+                message: "Failed to update kost: {$e->getMessage()}",
                 statusCode: Response::HTTP_INTERNAL_SERVER_ERROR
             );
         }
